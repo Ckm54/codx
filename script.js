2517adcb5ed2fcdcfbf3ba658c42500cbba7c2a2
collo <mutaic84@gmail.com>
2022-12-28 17:08:04 +0300

    :hammer: :lipstick: create chat stripes

--- script.js
+++ script.js
@@ -38,3 +38,18 @@ function generateUniqueId() {
 
   return `id-${timeStamp}-${hexadecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div class="profile">
+            <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}" />
+          </div>
+          <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
